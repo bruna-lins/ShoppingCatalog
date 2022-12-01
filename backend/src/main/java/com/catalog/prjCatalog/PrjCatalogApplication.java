2cd7c6f5c98327543d21c08787de6bfe2d96293c
= <=>
2022-12-01 16:24:56 -0300

    backend initial template

---
+++ backend/src/main/java/com/catalog/prjCatalog/PrjCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.catalog.prjCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/catalog/prjCatalog/PrjCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalog.prjCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
