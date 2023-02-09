3b35adf56f4ad2776d7363017e02438f132e4546
= <=>
2023-02-09 17:10:57 -0300

    pageable refactor

--- backend/src/main/java/com/catalog/prjCatalog/services/CategoryService.java
+++ backend/src/main/java/com/catalog/prjCatalog/services/CategoryService.java
@@ -1,8 +1,6 @@
 package com.catalog.prjCatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
