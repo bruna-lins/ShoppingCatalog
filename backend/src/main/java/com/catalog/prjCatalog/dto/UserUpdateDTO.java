eb5c9c224a4911b8990c75ac39c32e01295dc9b7
= <=>
2023-02-27 19:38:41 -0300

    add user update validation

---
+++ backend/src/main/java/com/catalog/prjCatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.catalog.prjCatalog.dto;
+
+import com.catalog.prjCatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+
+	private static final long serialVersionUID = 1L;
+
+}
