bbe1164243d7d3bc13d0f6c933f27c95c92713e5
= <=>
2023-02-27 19:41:50 -0300

    add update validation

--- backend/src/main/java/com/catalog/prjCatalog/resources/UserResource.java
+++ backend/src/main/java/com/catalog/prjCatalog/resources/UserResource.java
@@ -20,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.catalog.prjCatalog.dto.UserDTO;
 import com.catalog.prjCatalog.dto.UserInsertDTO;
+import com.catalog.prjCatalog.dto.UserUpdateDTO;
 import com.catalog.prjCatalog.services.UserService;
 
 @RestController
@@ -51,9 +52,9 @@ public class UserResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@Valid @PathVariable Long id, @RequestBody UserDTO dto) {
-		dto = service.update(id, dto);
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+		UserDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 
 	@DeleteMapping(value = "/{id}")
